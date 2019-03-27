6886b34aa51fe9de675efd0eeb4aaa032cbf4990
Remy Sharp <remy@remysharp.com>
2019-03-27 11:29:24 +0000

    chore: remove unneeded stuff

--- cli/cli.js
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env node
-// require('es6-promise').polyfill(); // jshint ignore:line
-require('./blog')();
--- cli/dirs.js
+++
@@ -1,6 +0,0 @@
-var path = require('path');
-
-module.exports = {
-  draft: path.resolve(process.cwd(), 'public', 'drafts'),
-  publish: path.resolve(process.cwd(), 'public', 'blog'),
-};
