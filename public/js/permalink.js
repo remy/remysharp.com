584045b8cc739f3c1f9087b63d715a089ee4fb73
Remy Sharp <remy@remysharp.com>
2019-03-27 11:56:48 +0000

    fix: some audit bits

--- public/js/permalink.js
+++ public/js/permalink.js
@@ -1,13 +1,14 @@
-(function () {
+(function() {
   /* eslint-env browser */
   'use strict';
 
   var className = 'anchor';
-  var idcache = {};
+  var idCache = {};
   var count = 0;
 
   function injectStyles() {
-    var css = ['.anchor {',
+    var css = [
+      '.anchor {',
       'height: 20px;',
       'width: 20px;',
       // 'display: block;',
@@ -23,35 +24,41 @@
       'height: 100%;',
       'background: transparent;',
       'color: #3C4342;',
-    '}',
-    '',
-    '.anchor:hover {',
-     'color: #3C4342;',
-    '}',
-    'h1,h2,h3,h4,h5,h6 { position: relative; }',
-    '',
-    'h1:hover .anchor span:before,',
-    'h2:hover .anchor span:before,',
-    'h3:hover .anchor span:before,',
-    'h4:hover .anchor span:before,',
-    'h5:hover .anchor span:before,',
-    'h6:hover .anchor span:before {',
+      '}',
+      '',
+      '.anchor:hover {',
+      'color: #3C4342;',
+      '}',
+      'h1,h2,h3,h4,h5,h6 { position: relative; }',
+      '',
+      'h1:hover .anchor span:before,',
+      'h2:hover .anchor span:before,',
+      'h3:hover .anchor span:before,',
+      'h4:hover .anchor span:before,',
+      'h5:hover .anchor span:before,',
+      'h6:hover .anchor span:before {',
       'content: "¶";',
       // 'position: absolute;',
       'left: 0px;',
       'top: 0;',
-    '}'].join('').replace(/\.anchor/g, '.' + className);
+      '}',
+    ]
+      .join('')
+      .replace(/\.anchor/g, '.' + className);
 
     var style = document.createElement('style');
     style.innerHTML = css;
     document.head.appendChild(style);
   }
 
-  function permalink(){
-    var $ = function (s, context) {
-      s = s.split(',').map(function (s) {
-        return '.post-content ' + s;
-      }).join(',');
+  function permalink() {
+    var $ = function(s) {
+      s = s
+        .split(',')
+        .map(function(s) {
+          return '.post-content ' + s;
+        })
+        .join(',');
       return document.querySelectorAll(s);
     };
 
@@ -59,19 +66,24 @@
     anchor.className = className;
     anchor.innerHTML = '<span></span>';
 
-    [].forEach.call($('h1,h2,h3,h4,h5,h6'), function (el) {
+    [].forEach.call($('h1,h2,h3,h4,h5,h6'), function(el) {
       if (!el.id) {
         // let's make one
-        var id = (el.textContent||el.innerText).replace(/&.*?;/g, '').replace(/\s+/g, '-').replace(/[^\w\-]/g, '').toLowerCase();
-        if (idcache[id]) {
+        var id = (el.textContent || el.innerText)
+          .replace(/&.*?;/g, '')
+          .replace(/\s+/g, '-')
+          .replace(/[^\w\-]/g, '')
+          .toLowerCase();
+        if (idCache[id]) {
           id = id + '-' + count;
         }
         el.id = id;
-        idcache[id] = 1;
+        idCache[id] = 1;
       }
 
       var clone = anchor.cloneNode(true);
       clone.href = '#' + el.id;
+      clone.setAttribute('aria-label', 'Link to ' + el.textContent);
       el.appendChild(clone, el.firstChild);
       count = count + 1;
     });
