e9ca439fae2e206c708e6284f4a439ec73b88d00
Remy Sharp <remy@remysharp.com>
2020-09-30 18:45:43 +0100

    imatwit

--- bin/hashit.sh
+++ bin/hashit.sh
@@ -1,1 +1,1 @@
-echo "console.info('Release https://github.com/remy/zx-tools/compare/$CACHED_COMMIT_REF...$COMMIT_REF');" > ./public/js/version.js
+echo "console.info('Release https://github.com/remy/remysharp.com/compare/$CACHED_COMMIT_REF...$COMMIT_REF');" > ./public/js/version.js
